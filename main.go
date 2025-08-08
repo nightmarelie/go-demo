9e387d57fd47f5b5486d4abe45da23d2d897a177
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-08-07 22:13:11 -0600

    fix: update user height and weight for accurate BMI calculation

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	var userHeight = 70.0
-	var userKg float64 = 86
+	var userHeight = 1.78
+	var userKg float64 = 85.5
 	var IMT = userKg / math.Pow(userHeight, 2)
 
 	fmt.Println(IMT)
