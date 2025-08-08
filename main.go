cc4234bea8dbde0eaf286faed3d2a2dbc041e896
Oleksii Kryvtsov <nightmarelie@gmail.com>
2025-08-07 22:11:05 -0600

    fix: update BMI calculation to use height squared for accuracy

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	var userHeight = 70.0
 	var userKg float64 = 86
-	var IMT = userKg / userHeight
+	var IMT = userKg / math.Pow(userHeight, 2)
 
 	fmt.Println(IMT)
 }
